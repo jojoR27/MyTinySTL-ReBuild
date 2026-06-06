abc17082e67a9b63b06519b4a95beb1f2b0aad2e
jojoR27 <2863963537@qq.com>
2026-06-06 10:13:08 +0800

    feat: 实现 uninitialized.h 未初始化内存工具

--- src/uninitialized.h
+++ src/uninitialized.h
@@ -51,6 +51,97 @@ namespace mytiny
         using isPOD  = typename type_traits<ValType>::is_POD_type;
         return unchecked_uninit_copy(first, last, result, isPOD());
     }
+
+
+    // 容器“初始化”函数，eg:vector<int>v1(10, 9)
+    // 未初始化空间上填充10个9  填充n个value
+    // POD类型
+    template <class ForwardIter, class Size, class T>
+    ForwardIter
+    unchecked_uninit_fill_n(ForwardIter first, Size n, const T& value, _true_type)
+    {
+        for(Size i = 0; i<n; ++i,++first)
+        {
+            *first = value;
+        }
+        return first;
+    }
+
+    // 非POD类型
+    template <class ForwardIter, class Size, class T>
+    ForwardIter
+    unchecked_uninit_fill_n(ForwardIter first, Size n, const T& value, _false_type)
+    {
+        ForwardIter cur = first;
+        try
+        {
+            {
+                for(Size i =0; i<n; ++i, ++cur)
+                {
+                    construct(&*cur, value);
+                }
+            }
+        }
+        catch(...)
+        {
+            destory(first, cur);
+            throw;
+        }
+        return cur;
+    }
+
+    // 统一接口
+    template <class ForwardIter, class Size, class T>
+    ForwardIter
+    unchecked_uninit_fill_n(ForwardIter first, Size n, const T& value)
+    {
+        using ValType = typename iterator_traits<ForwardIter>::value_type;
+        using isPOD = typename type_traits<ValType>::is_POD_type;
+        return unchecked_uninit_fill_n(first, n, value, isPOD());
+    }
+
+
+    // 未初始化区间[first,last)填充value
+    // POD类型
+    template <class ForwardIter, class T>
+    void 
+    unchecked_uninit_fill(ForwardIter first, ForwardIter last, const T& value, _true_type)
+    {
+        for (; first != last; ++first)
+        {
+            *first = value;
+        }
+    }
+
+    // 非POD类型
+    template <class ForwardIter, class T>
+    void 
+    unchecked_uninit_fill(ForwardIter first, ForwardIter last, const T& value, _false_type)
+    {
+        ForwardIter cur = first;
+        try
+        {
+            for (; cur != last; ++cur)
+            {
+                construct(&*cur, value);
+            }
+        }
+        catch (...)
+        {
+            destroy(first, cur);
+            throw;
+        }
+    }
+
+    // 统一接口
+    template <class ForwardIter, class T>
+    void 
+    uninitialized_fill(ForwardIter first, ForwardIter last, const T& value)
+    {
+        using ValType = typename iterator_traits<ForwardIter>::value_type;
+        using isPOD = typename type_traits<ValType>::is_POD_type;
+        unchecked_uninit_fill(first, last, value, isPOD());
+    }
 }
 
 #endif
